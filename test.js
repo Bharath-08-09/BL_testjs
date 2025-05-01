595838cb4549706000536af1793ae5c3491328d7
BL_08 <vsbharath2002@gmail.com>
2025-05-01 13:25:23 +0530

    second commit

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("greetButton").addEventListener("click", function () {
       const name = document.getElementById("nameInput").value.trim();
-      const message = name ? `Hello, ${name}! Nice to meet you.` : "Please enter your name.";
+      const message = name ? `Hello, ${name}! Nice to meet you.` : "Please enter your name, for your sake.";
       document.getElementById("greeting").innerText = message;
     });
   });  
